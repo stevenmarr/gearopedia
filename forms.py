ea93a52f28b15072939efcf96f7d64c8ac556ecc
Steven Marr(w) <Work@Stevens-MacBook-Pro.local>
2015-03-14 09:23:06 -0600

    Initial commit

---
+++ forms.py
@@ -0,0 +1,20 @@
+from wtforms import Form, BooleanField, StringField, validators, TextField, PasswordField, DateField, SelectField
+from wtforms.widgets.core import Select
+from wtforms import widgets
+
+class AddCategoryForm(Form):
+    name = StringField('Category Name',
+                          [validators.Required(), validators.Length(1,20)])
+    
+class AddItemForm(Form):
+	manufacturer = StringField('Manufacturer',
+                         	[validators.Required(), validators.Length(4,80)])
+	model = StringField('Model',
+							[validators.Required(), validators.Length(3,80)])
+	description = StringField('Description',
+							[validators.Length(0,800)])
+	product_url = StringField('Model Website',
+							[validators.URL(require_tld=False, message='Invalid URL')])
+	manual_url = StringField('Link to owners manual',
+							[validators.URL(require_tld=False, message='Invalid URL')])
+	
