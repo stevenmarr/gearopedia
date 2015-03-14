9f0d58a40b77326da936dbf6075fc1e647320073
Steven Marr(w) <Work@Stevens-MacBook-Pro.local>
2015-03-14 10:56:28 -0600

    Implement AddModel handler, change handler descriptions replace 'item' with 'model' (better real world accuraccy)

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 from models import GearCategories, GearModels, Base
 
-from forms import AddCategoryForm, AddItemForm
+from forms import AddCategoryForm, AddModelForm
 
 app = Flask(__name__)
 engine = create_engine('sqlite:///gear_wiki.db')
@@ -39,20 +39,38 @@ def DeleteGearCategory(category_id):
 	pass
 
 @app.route('/view_items/<int:category_id>/')
-def ViewItems(category_id):
-	items = session.query(GearModels).filter_by(category_id=category_id).all()
-	return render_template('items.html', items=items)
+def ViewModels(category_id):
+	models = session.query(GearModels).filter_by(category_id=category_id).all()
+	return render_template('view_models.html', 
+							models=models,
+							category_id=category_id)
 
 @app.route('/add_item/<int:category_id>/', methods=['GET','POST'])
-def AddItem(category_id):
+def AddModel(category_id):
+	category = session.query(GearCategories).filter_by(id=category_id).one()
+	if request.method == 'POST':
+		form = AddModelForm(request.form)
+		if not form.validate():
+			return render_template('add_model.html', form=form, category=category)
+		new_model = GearModels()
+		form.populate_obj(new_model)
+		new_model.category = category
+		session.add(new_model)
+		session.commit()
+		flash('New model created')
+		return redirect(url_for('DefaultGearCategories'))
+	else:
+		form = AddModelForm()
+		
+		return render_template('add_model.html', form=form, category=category)
 	pass
 
-@app.route('/edit_item/<int:item_id>/', methods=['GET','POST'])
-def EditItem(item_id):
+@app.route('/edit_model/<int:model_id>/', methods=['GET','POST'])
+def EditModel(model_id):
 	pass
 
-@app.route('/delete_item/<int:item_id>/', methods=['GET','POST'])
-def DeleteItem(item_id):
+@app.route('/delete_model/<int:model_id>/', methods=['GET','POST'])
+def DeleteModel(model_id):
 	pass
 
 if __name__ == '__main__':
