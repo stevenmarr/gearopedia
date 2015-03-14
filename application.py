353ad53917a3557f3cf014253e23b058cd63bbf1
Steven Marr(w) <Work@Stevens-MacBook-Pro.local>
2015-03-14 09:21:33 -0600

    Add handlers

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 from models import GearCategories, GearModels, Base
 
-#from wtforms import *
+from forms import AddCategoryForm, AddItemForm
 
 app = Flask(__name__)
 engine = create_engine('sqlite:///gear_wiki.db')
@@ -19,10 +19,31 @@ def DefaultGearCategories():
 	categories = session.query(GearCategories).all()
 	return render_template('default.html', categories=categories)
 
-@app.route('/view_items/<int:category_id>')
+@app.route('/add_category/')
+def AddGearCategory():
+	
+	pass
+
+@app.route('/delete_category/<int:category_id>/')
+def DeleteGearCategory(category_id):
+	pass
+
+@app.route('/view_items/<int:category_id>/')
 def ViewItems(category_id):
 	items = session.query(GearModels).filter_by(category_id=category_id).all()
 	return render_template('items.html', items=items)
+
+@app.route('/add_item/<int:category_id>/')
+def AddItem(category_id):
+	pass
+
+@app.route('/edit_item/<int:item_id>/')
+def EditItem(item_id):
+	pass
+
+@app.route('/delete_item/<int:item_id>/')
+def DeleteItem(item_id):
+	pass
 
 if __name__ == '__main__':
 	app.secret_key = 'super-secret-key'
