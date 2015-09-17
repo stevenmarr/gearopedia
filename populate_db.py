eb828b1a34c49ef95223b9bc1470d499f53fb4aa
Steven Marr <stevenmarr@me.com>
2015-09-17 19:18:08 +0000

    Test function

--- populate_db.py
+++ populate_db.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
  
 from models import GearCategories, GearModels, Base
-
-engine = create_engine('postgresql://postgres:marr@localhost/gearopedia') 
-#engine = create_engine('sqlite:///gear_wiki.db')
+from setting import DATA_BASE 
+engine = create_engine(DATA_BASE)
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
