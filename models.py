5ac9d11113cf5a53275a90b6d40c0e55f526df4d
Steven Marr <stevenmarr@me.com>
2015-09-17 19:17:31 +0000

    Remove Users, change user_id from foreign_key type to string type

--- models.py
+++ models.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from setting import DATA_BASE
 
 Base = declarative_base()
-
-
-class Users(Base):
-
-    __tablename__ = 'user'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    picture = Column(String(250))
 
 
 class GearCategories(Base):
@@ -26,8 +16,7 @@ class GearCategories(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(20), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(Users)
+    user_id = Column(String, nullable=False)
 
 
 class GearModels(Base):
@@ -42,12 +31,8 @@ class GearModels(Base):
     manual_url = Column(String(80))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(GearCategories)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(Users)
+    user_id = Column(String, nullable=False)
     image_path = Column(String(250))
-
-    # manual = Column(LargeBinary, nullable=True)
-
     @property
     def serialize(self):
         return {
@@ -56,34 +41,33 @@ class GearModels(Base):
             'description': self.description,
             'website': self.product_url,
             'category': self.category.name,
+	    'user_id': self.user_id	
             }
 
 
 class Images(Base):
 
     __tablename__ = 'image'
+
     id = Column(Integer, primary_key=True)
     file_name = Column(String(80), nullable=False)
     path = Column(String(250), nullable=False)
     model_id = Column(Integer, ForeignKey('model.id'))
     model = relationship(GearModels)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(Users)
-
+    user_id = Column(String, nullable=False)
 
 class UploadedFiles(Base):
 
     __tablename__ = 'file'
+    
     id = Column(Integer, primary_key=True)
     file_name = Column(String(80), nullable=False)
     file_type = Column(String(80), nullable=False)
     path = Column(String(250), nullable=False)
     model_id = Column(Integer, ForeignKey('model.id'))
     model = relationship(GearModels)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(Users)
+    user_id = Column(String, nullable=False)	
 
 
-#engine = create_engine('sqlite:///gear_wiki.db')
-engine = create_engine('postgresql://postgres:marr@localhost/gearopedia')
+engine = create_engine(DATA_BASE)
 Base.metadata.create_all(engine)
