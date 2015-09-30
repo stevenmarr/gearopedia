c7ae5ecef58b076e6c6db1263151a2d5ee91f4de
Steven Marr <stevenmarr@me.com>
2015-09-30 17:30:31 -0600

    Update postgres credentials

--- config/production.py
+++ config/production.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 DEBUG = False
+#SERVER_NAME = 'http://ec2-52-88-73-74.us-west-2.compute.amazonaws.com'
 SQLALCHEMY_DATABASE_URI= \
-'postgresql://postgres:marr@localhost/gearopedia'
+'postgresql://catalog:udacity@localhost/gearopedia'
 
