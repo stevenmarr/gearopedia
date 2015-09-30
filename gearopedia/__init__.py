70151d8ed22de2e47d44689ce071271709691652
System Administrator <stevenmarr@me.com>
2015-09-29 21:10:38 -0600

    Allow for loading production settings

--- gearopedia/__init__.py
+++ gearopedia/__init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 from flask import Flask
+#import gearopedia.views
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
-app.config.from_pyfile('config.py') 
+app.config.from_pyfile('config.py')
+app.config.from_envvar('APP_CONFIG', silent=True)
 
-from gearopedia import views
+import gearopedia.views
+
