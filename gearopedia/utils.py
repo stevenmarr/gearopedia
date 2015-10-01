caa528e65f396217d4dc7c3c63e86854d0d9d4f8
stevenmarr <stevenmarr@me.com>
2015-09-30 23:26:29 -0600

    Add comments, PEP8 formatting, remove try except from delete_files, add exception handlers

--- gearopedia/utils.py
+++ gearopedia/utils.py
@@ -14,21 +14,29 @@ from forms import FILE_TYPE
 ALLOWED_IMG_EXTENSIONS = app.config['ALLOWED_IMG_EXTENSIONS']
 ALLOWED_EXTENSIONS = app.config['ALLOWED_EXTENSIONS']
 
-# file helper functions
+
 def allowed_file(filename):
-    """Return filename if extension is in ALLOWED_EXTENSIONS"""
+    """Returns filename if the filename extension is in ALLOWED_EXTENSIONS."""
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 def add_file(upload_file, file_type, model_id, edit=None):
-    """given a file, a file type and a model id place the file in the database, replacing existing file_types.
-    ........Return the path to the new file"""
+    """
+    Adds file to database, and writes file to server.
+    If the model already has a file of the same type, the old file
+    is deleted in the database and from the server and the new file
+    is written.
+
+    Keyword arguments
+    edit -- a flag indicated the function was called from and edit
+            operation
+    """
 
     # Query existing files
     if edit:
         model_files = \
             session.query(UploadedFiles).filter_by(model_id=model_id)
-    map(delete_file, [model_file.id for model_file in model_files if model_file.file_type == FILE_TYPE[file_type]])
+        map(delete_file, [model_file.id for model_file in model_files if model_file.file_type == FILE_TYPE[file_type]])
 
     # check filename extension and make secure
     if not allowed_file(upload_file.filename):
@@ -43,48 +51,56 @@ def add_file(upload_file, file_type, model_id, edit=None):
     # Create new UploadedFiles object
     path = url_for('uploaded_file', filename=filename)
     model_file = UploadedFiles(file_name=filename,
-                               	file_type=FILE_TYPE[file_type],
-				model_id = model_id,
-    				user_id = login_session['name'],
-    				path = path)
+                               file_type=FILE_TYPE[file_type],
+                               model_id=model_id,
+                               user_id=login_session['name'],
+                               path=path)
     session.add(model_file)
     session.commit()
     return path
 
 
 def delete_file(id):
-    """For a given file id delete the file"""
+    """Delete the file from the database and server.
+
+    Keyword arguments
+    id --- database id of the uploaded file
+    """
     uploaded_file = session.query(UploadedFiles).filter_by(id=id).one()
     session.delete(uploaded_file)
     session.commit()
     try:
         os.remove(os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.file_name))
-    except:
+    except OSError:
         logging.error("File does not exist")
 
 
 def delete_files(model_id):
-    """for a given model_id, delete all its files"""
-    try:
-        files = session.query(UploadedFiles).filter_by(model_id=model_id)
-        # delete files if they exist
-        if files:
-            map(delete_file, [f.id for f in files])
-    except:
-        logging.info("No files exist for deletion")
+    """Delete all files associated with model.
+
+    Keyword arguments
+    model_id --- database id of the model to delete files from
+    """
+
+    files = session.query(UploadedFiles).filter_by(model_id=model_id)
+    # delete files if they exist
+    if files:
+        map(delete_file, [f.id for f in files])
 
 
-# image helper functions
 def allowed_image(filename):
-    """Return filename if extension is in ALLOWED_IMG_EXTENSIONS"""
-
+    """Returns filename if the filename extension is in ALLOWED_IMG_EXTENSIONS."""
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_IMG_EXTENSIONS
 
 
 def add_image(upload_image, model_id):
-    """Given an upload_image and model_id, assign the upload_image to the model"""
+    """Upload image file and tag the image to the model.
+
+    Keyword arguments
+    upload_image --- image file to upload
+    model_id -- database id of the model"""
+
     # Erase existing upload_image if it exists
-    model = session.query(GearModels).filter_by(id=model_id)
     delete_image(model_id)
     # check filename extension and make secure
     if not allowed_image(upload_image.filename):
@@ -107,24 +123,26 @@ def add_image(upload_image, model_id):
 
 
 def delete_image(model_id):
-    """Given a model_id erase the stored image"""
+    """Erase images file for model.
+
+    Keyword arguments
+    model_id ---  database id of the model"""
     try:
         image = session.query(Images).filter_by(model_id=model_id).one()
         # delete image
         if image:
             try:
                 os.remove(os.path.join(app.config['UPLOAD_IMG_FOLDER'], image.file_name))
-            except:
+            except OSError:
                 logging.info("File did not exist")
             session.delete(image)
             session.commit()
-    except:
+    except sqlalchemy.orm.exc.NoResultFound:
         logging.info("No images exist")
 
 
-# login check
 def check_login():
-    """check for valid login"""
+    """Return user name if user is logged in"""
     if 'name' not in login_session:
         raise TypeError
     else:
