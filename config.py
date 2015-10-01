21ebf2bdd98dc71fff077b7a0a7a46534a696d0c
stevenmarr <stevenmarr@me.com>
2015-09-30 23:16:45 -0600

    Apply PEP8, remove uneeded comments

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
-#config.py
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 APPLICATION_NAME = 'Gearopedia'
 DEBUG = True
 SECRET_KEY = 'secret_key'
-SQLALCHEMY_DATABASE_URI= \
-'sqlite:////%s/database/gearopedia.db' % BASE_DIR
-CLIENT_ID= \
-'522438111583-kpb1j28juerv3haeaak2qpq8qshetjtv.apps.googleusercontent.com'
+SQLALCHEMY_DATABASE_URI = \
+        'sqlite:////%s/database/gearopedia.db' % BASE_DIR
+CLIENT_ID = \
+    '522438111583-kpb1j28juerv3haeaak2qpq8qshetjtv.apps.googleusercontent.com'
 
 UPLOAD_FOLDER = '%s/files/uploads' % BASE_DIR
 UPLOAD_IMG_FOLDER = '%s/files/img' % BASE_DIR
