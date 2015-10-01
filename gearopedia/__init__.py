4293e3982bfe0835bc0dd2d1687da074d47cfb4c
stevenmarr <stevenmarr@me.com>
2015-09-30 23:17:52 -0600

    Add comment explaining envvar

--- gearopedia/__init__.py
+++ gearopedia/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 from flask import Flask
-#import gearopedia.views
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
-app.config.from_envvar('APP_CONFIG', silent=True)
+app.config.from_envvar('APP_CONFIG', silent=True)  # Loads config/production.py when APP_CONFIG is defined
 
 import gearopedia.views
 from database import init_db
+
 init_db()
