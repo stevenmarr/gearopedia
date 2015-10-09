c94bd6dc7a2bc448cadeb52e9ee37ef889bc6b21
Steven Marr <stevenmarr@me.com>
2015-10-08 18:34:49 -0600

    Remove session.close()

--- gearopedia/utils.py
+++ gearopedia/utils.py
@@ -57,7 +57,6 @@ def add_file(upload_file, file_type, model_id, edit=None):
                                path=path)
     session.add(model_file)
     session.commit()
-    session.close()
     return path
 
 
@@ -70,7 +69,6 @@ def delete_file(id):
     uploaded_file = session.query(UploadedFiles).filter_by(id=id).one()
     session.delete(uploaded_file)
     session.commit()
-    session.close()
     try:
         os.remove(os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.file_name))
     except OSError:
@@ -121,7 +119,6 @@ def add_image(upload_image, model_id):
                    path=path)
     session.add(image)
     session.commit()
-    session.close()
     return path
 
 
