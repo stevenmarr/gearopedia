94370edc827ab03b8231e9e333b1d4b0ae3dadff
Steven Marr <stevenmarr@me.com>
2015-10-11 14:17:39 -0600

    Add db. infront on field definitions

--- gearopedia/models.py
+++ gearopedia/models.py
@@ -11,9 +11,9 @@ class GearCategories(db.Model):
 
     __tablename__ = 'category'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(20), nullable=False)
-    user_id = Column(String, nullable=False)
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(20), nullable=False)
+    user_id = db.Column(String, nullable=False)
 
     def __repr__(self):
         return '<Category name: %r, User ID: %r>' % (self.name, self.user_id)
@@ -24,16 +24,16 @@ class GearModels(db.Model):
 
     __tablename__ = 'model'
 
-    id = Column(Integer, primary_key=True)
-    manufacturer = Column(String(80), nullable=False)
-    name = Column(String(80), nullable=False)
-    description = Column(String(800))
-    product_url = Column(String(2084))
-    manual_url = Column(String(2084))
-    category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(GearCategories)
-    user_id = Column(String, nullable=False)
-    image_path = Column(String(250))
+    id = db.Column(Integer, primary_key=True)
+    manufacturer = db.Column(String(80), nullable=False)
+    name = db.Column(String(80), nullable=False)
+    description = db.Column(String(800))
+    product_url = db.Column(String(2084))
+    manual_url = db.Column(String(2084))
+    category_id = db.Column(Integer, ForeignKey('category.id'))
+    category = db.relationship(GearCategories)
+    user_id = db.Column(String, nullable=False)
+    image_path = db.Column(String(250))
 
     def __repr__(self):
         return '<Model Name: %r, User ID: %r>' % (self.name, self.user_id)
@@ -58,12 +58,12 @@ class Images(db.Model):
 
     __tablename__ = 'image'
 
-    id = Column(Integer, primary_key=True)
-    file_name = Column(String(80), nullable=False)
-    path = Column(String(250), nullable=False)
-    model_id = Column(Integer, ForeignKey('model.id'))
-    model = relationship(GearModels)
-    user_id = Column(String, nullable=False)
+    id = db.Column(Integer, primary_key=True)
+    file_name = db.Column(String(80), nullable=False)
+    path = db.Column(String(250), nullable=False)
+    model_id = db.Column(Integer, ForeignKey('model.id'))
+    model = db.relationship(GearModels)
+    user_id = db.Column(String, nullable=False)
 
     def __repr__(self):
         return '<File name: %r, User ID: %r>' % (self.file_name, self.user_id)
@@ -74,13 +74,13 @@ class UploadedFiles(db.Model):
 
     __tablename__ = 'file'
 
-    id = Column(Integer, primary_key=True)
-    file_name = Column(String(80), nullable=False)
-    file_type = Column(String(80), nullable=False)
-    path = Column(String(250), nullable=False)
-    model_id = Column(Integer, ForeignKey('model.id'))
-    model = relationship(GearModels)
-    user_id = Column(String, nullable=False)
+    id = db.Column(Integer, primary_key=True)
+    file_name = db.Column(String(80), nullable=False)
+    file_type = db.Column(String(80), nullable=False)
+    path = db.Column(String(250), nullable=False)
+    model_id = db.Column(Integer, ForeignKey('model.id'))
+    model = db.relationship(GearModels)
+    user_id = db.Column(String, nullable=False)
 
     def __repr__(self):
         return '<File name: %r, User ID: %r>' % (self.file_name, self.user_id)    
