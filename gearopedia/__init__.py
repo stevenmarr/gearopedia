8771f84cf76640a905f538ba35eca979a6388c66
Steven Marr <stevenmarr@me.com>
2015-10-13 11:00:01 -0600

    Minor edits

--- gearopedia/__init__.py
+++ gearopedia/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
-from flash.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
-app.config.from_envvar('APP_CONFIG', silent=True)  # Loads config/production.py when APP_CONFIG is defined
+# app.config.from_envvar('APP_CONFIG', silent=True)  # Loads config/production.py when APP_CONFIG is defined
 
 db = SQLAlchemy(app)
 
-from gearopedia import views, models
+import views, models
 # import gearopedia.views
 # from database import init_db
 
