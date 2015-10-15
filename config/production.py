73b4a6a58478a069ca7f7a079468b68f42b4623c
Steven Marr <stevenmarr@me.com>
2015-10-15 09:19:18 -0600

    Add non working email attributes for app.config

--- config/production.py
+++ config/production.py
@@ -1,4 +1,11 @@
 DEBUG = False
 SQLALCHEMY_DATABASE_URI= \
 'postgresql://catalog:udacity@localhost/gearopedia'
+#mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
 
+# adminstrators
+ADMINS = ['stevenmarr@me.com']
