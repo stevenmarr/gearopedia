19c109f2c941423c9694ea4fe54f56f1d09f4559
Steven Marr <stevenmarr@me.com>
2015-10-15 14:38:18 -0600

    Move attributes to devevelopment.py

--- config.py
+++ config.py
@@ -2,10 +2,6 @@
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 APPLICATION_NAME = 'Gearopedia'
-DEBUG = True
-SECRET_KEY = 'secret_key'
-SQLALCHEMY_DATABASE_URI = \
-    'sqlite:///' + os.path.join(BASE_DIR, 'gearopedia.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 CLIENT_ID = \
     '522438111583-kpb1j28juerv3haeaak2qpq8qshetjtv.apps.googleusercontent.com'
