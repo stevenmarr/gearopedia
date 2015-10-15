1acdc496152ef10d357abeb01284e40c1eca08e2
stevenmarr <stevenmarr@me.com>
2015-10-15 16:56:15 -0600

    Add env var pointing to development config

--- run.py
+++ run.py
@@ -3,7 +3,10 @@
 # Run a test server.
 import os
 
-os.environ['APP_CONFIG'] = '/var/www/gearopedia/config/development.py'
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+os.environ['APP_CONFIG'] = '%s/config/development.py' % BASE_DIR
 
 from gearopedia import app
 app.run(host='127.0.0.1', port=8080, debug=True)
