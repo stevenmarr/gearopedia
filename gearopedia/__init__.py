2d1b85d60720395d4b73197a1c0ab19f84dcfc34
stevenmarr <stevenmarr@me.com>
2015-10-15 16:52:35 -0600

    Change app.config.from_envvar, silent to False, receive erros when envar config is not loaded

--- gearopedia/__init__.py
+++ gearopedia/__init__.py
@@ -8,7 +8,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
-app.config.from_envvar('APP_CONFIG', silent=True)  # Loads config/production.py when APP_CONFIG is defined
+app.config.from_envvar('APP_CONFIG', silent=False)  # Loads config/production.py when APP_CONFIG is defined
 
 
 db = SQLAlchemy(app)
