c585d40be58df3653062e913765e0f5ff36860e5
Steven Marr <stevenmarr@me.com>
2015-10-15 14:39:30 -0600

    Remove mail settings, update database uri

--- config/production.py
+++ config/production.py
@@ -1,11 +1,9 @@
+import sys
+sys.path.insert(0, '/var/www/gearopedia')
+
+from instance.config import db_pass
+
+
 DEBUG = False
 SQLALCHEMY_DATABASE_URI= \
-'postgresql://catalog:udacity@localhost/gearopedia'
-#mail server settings
-MAIL_SERVER = 'localhost'
-MAIL_PORT = 25
-MAIL_USERNAME = None
-MAIL_PASSWORD = None
-
-# adminstrators
-ADMINS = ['stevenmarr@me.com']
+'postgresql://postgres:%s@localhost/gearopedia' % db_pass
