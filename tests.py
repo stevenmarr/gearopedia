8f2cbcc5aca913de1b50e640ccd50c1f2f0a5732
stevenmarr <stevenmarr@me.com>
2015-10-14 23:46:44 -0600

    Add coverage

--- tests.py
+++ tests.py
@@ -3,22 +3,23 @@ import os
 import unittest
 
 from coverage import coverage
+cov = coverage(branch=True, omit=['flask/*', 'tests.py'])
+cov.start()
+
 
 from gearopedia import app, db
 from gearopedia.models import GearModels
 
 
 BASE_DIR = app.config['BASE_DIR']
-cov = coverage(branch=True, omit=['venv/*', 'tests.py'])
-cov.start()
-
 class TestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'test.db')
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            os.path.join(BASE_DIR, 'test.db')
         
-        self.app = app.test_client()
+        # self.app = app.test_client()
         db.create_all()
         
     def teadDown(self):
@@ -41,4 +42,6 @@ if __name__ == '__main__':
     cov.save()
     print('\n\nCoverage Report:\n')
     cov.report()
+    print("HTML version: " + os.path.join(BASE_DIR, "tmp/coverage/index.html"))
+    cov.html_report(directory='tmp/coverage')
     cov.erase()
