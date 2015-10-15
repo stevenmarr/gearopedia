3c4c94cac9b5d084975e5406d6e9c49483758886
stevenmarr <stevenmarr@me.com>
2015-10-15 16:50:07 -0600

    Remove executable path

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
 import os
+
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 APPLICATION_NAME = 'Gearopedia'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 CLIENT_ID = \
     '522438111583-kpb1j28juerv3haeaak2qpq8qshetjtv.apps.googleusercontent.com'
-
 UPLOAD_FOLDER = '%s/files/uploads' % BASE_DIR
 UPLOAD_IMG_FOLDER = '%s/files/img' % BASE_DIR
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip', 'dmg'}
