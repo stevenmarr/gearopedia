54ae63f2d3282e89f5620b32236627acfb4dfbef
Steven Marr <stevenmarr@me.com>
2015-10-15 14:45:18 -0600

    Add config/development for app.config attributes

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 #!flask/bin/python
 
 # Run a test server.
+import os
+
+os.environ['APP_CONFIG'] = '/var/www/gearopedia/config/development.py'
+
 from gearopedia import app
 app.run(host='127.0.0.1', port=8080, debug=True)
