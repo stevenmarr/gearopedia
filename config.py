089f9bbd045bbb83d12de246ddbe20de34736f09
Steven Marr <StevenMarr@me.com>
2016-03-25 21:30:20 -0600

    Create config, add BaseConfig class

--- config.py
+++ config.py
@@ -1,13 +1,23 @@
 import os
 
+class BaseConfig(object):
+    DEBUG = True
+    SECRET_KEY = 'secret_key'
+    SQLALCHEMY_DATABASE_URI = \
+        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'gearopedia.db')
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    APPLICATION_NAME = 'Gearopedia'
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
+    CLIENT_ID = \
+        '522438111583-kpb1j28juerv3haeaak2qpq8qshetjtv.apps.googleusercontent.com'
+    UPLOAD_FOLDER = '%s/files/uploads' % BASE_DIR
+    UPLOAD_IMG_FOLDER = '%s/files/img' % BASE_DIR
+    ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip', 'dmg'}
+    ALLOWED_IMG_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-APPLICATION_NAME = 'Gearopedia'
-SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
-CLIENT_ID = \
-    '522438111583-kpb1j28juerv3haeaak2qpq8qshetjtv.apps.googleusercontent.com'
-UPLOAD_FOLDER = '%s/files/uploads' % BASE_DIR
-UPLOAD_IMG_FOLDER = '%s/files/img' % BASE_DIR
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip', 'dmg'}
-ALLOWED_IMG_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+    ADMINS = []
+    MAIL_SERVER = ''
+    MAIL_PORT  = ''
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
 
