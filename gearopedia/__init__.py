bcf481122831305f30cc64f2454df4909a9fa448
Steven Marr <StevenMarr@me.com>
2016-03-27 19:05:25 -0600

    format code

--- gearopedia/__init__.py
+++ gearopedia/__init__.py
@@ -1,5 +1,6 @@
 import os
 import logging
+
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
@@ -24,7 +25,6 @@ app.register_blueprint(home_blueprint)
 
 import views, models
 
-
 BASE_DIR = app.config['BASE_DIR']
 ADMINS = app.config['ADMINS']
 MAIL_SERVER = app.config['MAIL_SERVER']
@@ -32,15 +32,14 @@ MAIL_PORT = app.config['MAIL_PORT']
 MAIL_USERNAME = app.config['MAIL_USERNAME']
 MAIL_PASSWORD = app.config['MAIL_PASSWORD']
 
-
 if not app.debug:
     # Email errors
     credentials = None
     if MAIL_USERNAME or MAIL_PASSWORD:
         credentials = (MAIL_USERNAME, MAIL_PASSWORD)
     mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT),
-    	'no-reply@' + MAIL_SERVER, ADMINS, 'gearopedia failure',
-    	credentials)
+                               'no-reply@' + MAIL_SERVER, ADMINS,
+                               'gearopedia failure', credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
     # Logging
@@ -54,7 +53,7 @@ if not app.debug:
 
 # create file and image upload folder structure if it does not exist
 img_path = "%s/files/img" % BASE_DIR
-try: 
+try:
     os.makedirs(img_path)
 except OSError:
     if not os.path.isdir(img_path):
@@ -65,9 +64,4 @@ try:
     os.makedirs(u_path)
 except OSError:
     if not os.path.isdir(u_path):
-        raise        
-
-# import gearopedia.views
-#from database import init_db
-
-#init_db()
+        raise
