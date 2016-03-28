8f301e541e852a1c0ef8bec03d0df60b8a3ef19e
Steven Marr <StevenMarr@me.com>
2016-03-27 22:47:07 -0600

    refactor out un-needed migration settings

--- db_create.py
+++ db_create.py
@@ -1,29 +1,10 @@
-#!venv/bin/python
+from gearopedia import db
 
-from migrate.versioning import api
-from gearopedia import app, db
-from gearopedia.models import GearCategories
-import os.path
-
-
+# create the database and the db table
 db.create_all()
 
-SQLALCHEMY_DATABASE_URI = app.config['SQLALCHEMY_DATABASE_URI']
-SQLALCHEMY_MIGRATE_REPO = app.config['SQLALCHEMY_MIGRATE_REPO']
+# insert data
 
 
-
-
-if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-else:
-    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
-
-categories = ["Video-Projectors", "Video-Switchers", "Video-Processors",
-                "Video-Screens", "Audio-Console", "Audio-Microphones"]
-
-for category in categories:
-    db.session.add(GearCategories(name='%s'% category,
-                                user_id="Default"))
+# commit the changes
 db.session.commit()
